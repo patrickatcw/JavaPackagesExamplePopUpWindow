f045dfbc67177a1f912390868792fba4a07acad6
patrickatcw <pj8392@yahoo.com>
2018-04-30 09:32:21 -0400

    program complete, functions, got my pop up!

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -5,6 +5,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //step 10 instance
+        MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+
+        //step 11 run
+
     }
 
 }
